4788a404a39232d13f6f41dd70a4ef20c62a2f3d
pdante1897 <bryanpaez.125@gmail.com>
2021-02-16 12:24:10 -0600

    Decodificacion de archivo Json
    Decodificacion y creación de lista doblemente enlazada.

---
+++ src/Dato/Structs.go
@@ -0,0 +1,91 @@
+package Dato
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type ArchivoJson struct {
+	Datos []struct {
+		Indice        string `json:"Indice"`
+		Departamentos []struct {
+			Nombre  string   `json:"Nombre"`
+			Tiendas []Tienda `json:"Tiendas"`
+		} `json:"Departamentos"`
+	} `json:"Datos"`
+}
+type Tienda struct {
+	Nombre       string `json:"Nombre"`
+	Descripcion  string `json:"Descripcion"`
+	Contacto     string `json:"Contacto"`
+	Calificacion int    `json:"Calificacion"`
+}
+
+func (this Tienda) ToString() string {
+	return ("Nombre: " + this.Nombre + " Descripcion: " + this.Descripcion + " Contacto: " + this.Contacto + " Calificacion: " + strconv.Itoa(this.Calificacion))
+}
+
+func Hola() {
+	fmt.Println("hola mundo")
+
+}
+
+type Nodo struct {
+	Tienda    Tienda
+	Anterior  *Nodo
+	Siguiente *Nodo
+}
+type ListaDoble struct {
+	Nombre       string
+	Calificacion int
+	Inicio       *Nodo
+	Fin          *Nodo
+}
+
+func (this *ListaDoble) Vacio() bool {
+	if this.Inicio == nil {
+		return true
+	} else {
+		return false
+	}
+
+}
+func (this *ListaDoble) Insertar(dato Tienda) {
+	aux := Nodo{
+		Tienda:    dato,
+		Siguiente: nil,
+		Anterior:  nil,
+	}
+	if this.Vacio() {
+		this.Inicio = &aux
+		this.Fin = this.Inicio
+	} else {
+		aux.Anterior = this.Fin
+		this.Fin.Siguiente = &aux
+		this.Fin = this.Fin.Siguiente
+
+	}
+}
+
+func (this ListaDoble) To_String() {
+	var aux *Nodo
+	aux = this.Inicio
+	for {
+		if this.Vacio() {
+			break
+		}
+		if aux == this.Inicio {
+			fmt.Println(aux.Tienda.ToString())
+			aux = aux.Siguiente
+		} else if aux.Siguiente != nil {
+			fmt.Println(aux.Tienda.ToString())
+			aux = aux.Siguiente
+		} else if aux == this.Fin {
+			fmt.Println(aux.Tienda.ToString())
+			break
+		} else {
+			break
+		}
+
+	}
+}
--- src/main.go
+++ src/main.go
@@ -1,6 +1,46 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
-	dato.hola()
+	"./Dato"
+	"github.com/gorilla/mux"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hola mundo")
+}
+
+func agregar(w http.ResponseWriter, r *http.Request) {
+	listad := new(Dato.ListaDoble)
+	archivo := new(Dato.ArchivoJson)
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Error al insertar mensaje")
+		return
+	}
+	json.Unmarshal(reqBody, &archivo)
+	for i := 0; i < len(archivo.Datos); i++ {
+		fmt.Fprintln(w, archivo.Datos[i].Indice)
+		for j := 0; j < len(archivo.Datos[i].Departamentos); j++ {
+			fmt.Fprintln(w, archivo.Datos[i].Departamentos[j].Nombre)
+			for k := 0; k < len(archivo.Datos[i].Departamentos[j].Tiendas); k++ {
+				fmt.Fprintln(w, archivo.Datos[i].Departamentos[j].Tiendas[k].ToString())
+				listad.Insertar(archivo.Datos[i].Departamentos[j].Tiendas[k])
+			}
+		}
+	}
+	listad.To_String()
+}
+
+func main() {
+	fmt.Println("un server papu")
+	router := mux.NewRouter()
+	router.HandleFunc("/", index).Methods("GET")
+	router.HandleFunc("/agregar", agregar).Methods("POST")
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
